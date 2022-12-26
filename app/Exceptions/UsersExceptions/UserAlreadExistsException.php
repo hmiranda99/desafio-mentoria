4b670817965855478296b59d940549722b515f17
Helena Miranda da Silva <hmiranda99@gmail.com>
2022-12-26 10:54:36 -0300

    feat: user exception

---
+++ app/Exceptions/UsersExceptions/UserAlreadExistsException.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Exceptions\UsersExceptions;
+
+use Exception;
+use Fig\Http\Message\StatusCodeInterface;
+
+class UserAlreadExistsException extends Exception
+{
+    public const MSG_USER_ALREADY_EXISTS = 'User already exists.';
+    
+    /**
+     * Report the exception.
+     *
+     * @return bool|null
+     */
+    public function report()
+    {
+        return false;
+    }
+ 
+    /**
+     * Render the exception into an HTTP response.
+     * This method returns an error message for users that already exist in database.
+     *
+     * @param  \Illuminate\Http\Request  
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return response()->json(["error" => true, "message" => static::MSG_USER_ALREADY_EXISTS], StatusCodeInterface::STATUS_CONFLICT);
+    }
+}
