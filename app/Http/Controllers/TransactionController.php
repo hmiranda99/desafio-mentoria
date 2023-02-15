2291d81f09d567d5b4ac4096596ca1b77ce5b57c
Helena Miranda da Silva <hmiranda99@gmail.com>
2023-02-15 10:55:23 -0300

    fix: transaction flow refactoring

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,44 +2,82 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\TransactionHelper;
+use App\Enums\OperationEnum;
+use App\Enums\TransactionStatusEnum;
 use App\Services\TransactionService;
+use App\Repositories\AccountRepository;
 use Fig\Http\Message\StatusCodeInterface;
 use App\Http\Requests\CreateTransactionDto;
-use App\Exceptions\ServicesExceptions\ServiceDownException;
-use App\Exceptions\UsersExceptions\UserCannotTransactException;
+use App\Repositories\TransactionRepository;
+use App\Exceptions\TransactionsExceptions\TransactionNotAuthorizedException;
+use App\Helpers\TransactionHelper;
 
 class TransactionController extends Controller
 {
+    protected $accountRepository;
     protected $transactionHelper;
+    protected $transactionRepository;
     protected $transactionService;
+    protected $validateTransaction;
 
     public function __construct(
+        AccountRepository $accountRepository,
         TransactionHelper $transactionHelper,
-        TransactionService $transactionService
+        TransactionRepository $transactionRepository,
+        TransactionService $transactionService,
+        ValidateTransaction $validateTransaction
     ) {
         $this->transactionHelper = $transactionHelper;
+        $this->accountRepository = $accountRepository;
+        $this->transactionRepository = $transactionRepository;
         $this->transactionService = $transactionService;
+        $this->validateTransaction = $validateTransaction;
     }
 
     public function createTransaction(CreateTransactionDto $createTransactionDto)
     {
-        $createTransactionDto->transactionHash = $this->transactionHelper->createHashTransaction();
-
-        if (!$this->transactionHelper->canTransact($createTransactionDto->payer)) {
-            $this->transactionService->cancelTransaction($createTransactionDto);
-            throw new UserCannotTransactException();
-        }
-
-        $this->transactionService->captureTransaction($createTransactionDto);
+        $this->validateTransaction->handle($createTransactionDto);
 
         if (!$this->transactionService->authorizeService()) {
-            $this->transactionService->errorTransaction($createTransactionDto);
-            throw new ServiceDownException();
+            $this->cancelTransaction($createTransactionDto);
+            throw new TransactionNotAuthorizedException();
         }
 
-        $this->transactionService->authorizeTransaction($createTransactionDto);
+        $this->authorizeTransaction($createTransactionDto);
 
         return response(null, StatusCodeInterface::STATUS_CREATED);
+    }
+
+    private function cancelTransaction(CreateTransactionDto $createTransactionDto)
+    {
+        return $this->transactionRepository->registerTransaction(TransactionStatusEnum::NOT, $createTransactionDto);
+    }
+
+    private function authorizeTransaction(CreateTransactionDto $createTransactionDto)
+    {
+        $this->prepareUsersBalance($createTransactionDto);
+        return $this->transactionRepository->registerTransaction(TransactionStatusEnum::AUT, $createTransactionDto);
+    }
+
+    private function prepareUsersBalance(CreateTransactionDto $createTransactionDto)
+    {
+        $this->updateUsersBalance(
+            $createTransactionDto->payer,
+            $createTransactionDto->value,
+            OperationEnum::SUB
+        );
+
+        $this->updateUsersBalance(
+            $createTransactionDto->payee,
+            $createTransactionDto->value,
+            OperationEnum::ADD
+        );
+    }
+    
+    private function updateUsersBalance(int $user, int $transactionValue, OperationEnum $operation)
+    {
+        $account = $this->accountRepository->getAccountByUserId($user);
+        $newBalance = $this->transactionHelper->withdrawOrAddBalance($operation, $account->balance, $transactionValue);
+        return $this->accountRepository->updateBalanceAccount($newBalance, $user);
     }
 }
